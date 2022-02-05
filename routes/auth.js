27dc759b05b1881014cc244d5f9392df902577ca
itsritiksharma <sritik216@gmail.com>
2022-02-06 01:10:59 +0530

    Added express-validator's validations on login and signup to check for email and password.

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,33 @@ const express = require("express");
 
 const router = express.Router();
 
+const { check, body } = require("express-validator");
+
 const authController = require("../controllers/auth");
 
 // /auth/signup => POST: "ADD USER IN DATABASE"
-router.post("/signup",authController.postSignup);
+router.post(
+  "/signup",
+  [
+    body("userEmail", "Enter valid email").isEmail(),
+    body("password", "Password should be of 6 characters long.").isLength({
+      min: 6,
+    }),
+  ],
+  authController.postSignup
+);
 
 // /auth/login => POST : "POST THE USER DATA TO GET THE auth FROM DATABASE"
-router.post("/login",  authController.postLogin);
+router.post(
+  "/login",
+  [
+    body("userEmail", "Email is not valid.").isEmail(),
+    body("password", "Password should be of 6 characters long.").isLength({
+      min: 6,
+    }),
+  ],
+  authController.postLogin
+);
 
 // /auth/logout => POST : "LOG THE USER OUT BY SETTING THE TOKEN TO NULL"
 router.post("/logout", authController.postLogout);
