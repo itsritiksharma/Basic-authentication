f58a1f3d3d9dc7826426d6234f346b69caef5cef
itsritiksharma <sritik216@gmail.com>
2022-02-06 00:40:03 +0530

    Added authentication routes i.e. Login, Signup and Logout.

---
+++ routes/auth.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const router = express.Router();
+
+// /auth/signup => POST: "ADD USER IN DATABASE"
+router.post("/signup",authController.postSignup);
+
+// /auth/login => POST : "POST THE USER DATA TO GET THE auth FROM DATABASE"
+router.post("/login",  authController.postLogin);
+
+// /auth/logout => POST : "LOG THE USER OUT BY SETTING THE TOKEN TO NULL"
+router.post("/logout", authController.postLogout);
+
+module.exports = router;
